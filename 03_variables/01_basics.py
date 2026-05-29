da21aab0a1e30b9d86677ef968d30c14f2a8b15b
Mohit Kumar Shaw <mohitshaw188@gmail.com>
2026-05-29 17:59:50 +0530

    Concept of Variable in Python
    Practice and mastery of Python variables and basic syntax concepts.

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,18 @@
+# In python , variables are usedto store data that can used and manipulated throughout a program. A variable iscreated the moment you assign  Vlue to it using the assignment operator (=)
+
+# Python is a dynamically typed language and C is a static typed language
+
+age = 34 # integer
+name = "Harry" # String
+cgpa = 4.55 # float
+
+# Rules of defining a vaiable in python
+# Variable names must start with a letter (a-z ,A-Z) or an underscore(_).
+# Theycan contain letters , numbers and underscores.
+# Variables names are case_sensitive (age and Age are different).
+# Aoid using Python keywords (e.g if , for , while) as variable names
+
+#34age = 34 #invalid
+age= 34
+age___ = 34
+#a$$ge = 34
