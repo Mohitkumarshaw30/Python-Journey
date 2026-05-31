fe88e7702a9d50890aeffdccecf57fb29280487c
Mohit Kumar Shaw <mohitshaw188@gmail.com>
2026-05-31 12:34:44 +0530

    Comments , Escape and Print statement concept

---
+++ 06_comments_print_esc/01_comments.py
@@ -0,0 +1,24 @@
+print("How are you?")
+
+# Arjun please finish this code 
+
+# Rohan , please review what arjun has done
+
+#vMulti line comments
+
+'''
+List Indexing
+A list can be indexed just like a string.
+l1 = [7,9,"harry"]
+l1[0] # 7
+l1[1] # 9
+l1[2] # harry
+l1[0:2] # [7,9]  # list slicing
+List Methods
+Consider the following list.
+l1 = [1,8,7,2,21,15]
+l1.sort(): updates the list to [1,2,7,8,15,21]
+l1.reverse(): updates the list to [15,21,2,7,8,1]
+l1.append(8): adds 8 at the end of the list
+l1.insert(3,8): This will add 8 at 3 index
+'''
---
+++ 06_comments_print_esc/02_escape.py
@@ -0,0 +1,3 @@
+print("Hey how are you \ni am good \\newline")
+
+print("Hello \"world\"")
